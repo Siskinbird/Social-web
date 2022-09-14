a6a284e828182c743a0230a9043710e3084d8fae
Dmitrii Chizhov <dimachig@gmail.com>
2022-09-14 16:02:03 +0300

    add: main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,19 @@ import React from "react";
 
 function Main() {
     return (
-        <main className='content'>This is content block</main>
+        <main className='content'>
+            <div className="head-img"></div>
+            <div className="author__body">
+                <img className="author__body-avatar" alt="Avatar" />
+                <div className="author__body-description">
+                    <h2>Dmitry Chizhov</h2>
+                    <p>Date of burn: 12.10.1986</p>
+                    <p>City: Moscow</p>
+                    <p>Education: Crunch univercity</p>
+                    <p>Web Site: <a href="https://dmich.ru">dmich.ru</a></p>
+                </div>
+            </div>
+        </main>
     )
 }
 
