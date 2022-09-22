f98dcc4646f0eb49a4383ab67b0c08be2d9c0557
Dmitrii Chizhov <dimachig@gmail.com>
2022-09-22 14:52:22 +0300

    add: profile info component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,35 +1,27 @@
 import React from "react";
-import style from './Dialogs.module.css'
+import style from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+import Dialog from './Dialog/Dialog';
+import Message from './Message/Message';
 
 function Dialogs(props) {
     return (
         <section className={style.dialogs}>
             <div className={style.dialogs_items}>
-                <div className={style.dialog}>
-                    Ivan
-                </div>
-                <div className={style.dialog + ' ' + style.active}>
-                    Divan
-                </div>
-                <div className={style.dialog}>
-                    Oleg
-                </div>
-                <div className={style.dialog}>
-                    Sanya
-                </div>
-                <div className={style.dialog}>
-                    Grirory
-                </div>
-                <div className={style.dialog}>
-                    Zafir
-                </div>
+                <Dialog name='Ivan' id='1' />
+                <Dialog name='Zubr' id='2' />
+                <Dialog name='Olya' id='3' />
+                <Dialog name='Dima' id='4' />
+                <Dialog name='Sveta' id='5' />
+                <Dialog name='Katya' id='6' />
             </div>
             <div className={style.messages}>
-                <div className={style.message}>Hello</div>
-                <div className={style.message}>How are you</div>
-                <div className={style.message}>War is coming</div>
-                <div className={style.message}>Run from contry</div>
-                <div className={style.message}>End is hear</div>
+                <Message message='Hello'/>
+                <Message message='Hello'/>
+                <Message message='Hello'/>
+                <Message message='Hello'/>
+                <Message message='Hello'/>
+                <Message message='Hello'/>
             </div>
             <h2>Dialogs</h2>
         </section>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,21 +1,12 @@
 import React from "react";
 import MyPosts from './MyPosts/MyPosts'
 import style from './Main.module.css'
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 function Main() {
     return (
         <main>
-            <div className={style.head__img}></div>
-            <div className={style.author__body}>
-                <div className={style.author__avatar}/>
-                <div className={style.author__description}>
-                    <h2>Dmitry Chizhov</h2>
-                    <p>Date of burn: 12.10.1986</p>
-                    <p>City: Moscow</p>
-                    <p>Education: Crunch univercity</p>
-                    <p>Web Site: <a href="https://dmich.ru">dmich.ru</a></p>
-                </div>
-            </div>
+            <ProfileInfo />
             <MyPosts />
         </main>
     )
